17e396dc1cfbfdb5baad3f80102ab1be4381c333
rightbear <jjj17041113@gmail.com>
2025-07-09 21:24:10 +0800

    test: test append and toString functions in main.js

---
+++ functions/main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+import { LinkedList } from "./classes.js"
+
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+// test function append()
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+// test function toStrung()
+console.log(list.toString());
