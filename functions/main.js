2c5384b17f64fdb393e7a48df3e6315e0908a3d1
rightbear <jjj17041113@gmail.com>
2025-07-09 22:23:00 +0800

    test: test prepend, size, head, tail and at functions in main.js

--- functions/main.js
+++ functions/main.js
@@ -13,6 +13,28 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
+// test function prepend()
+list.prepend("fish");
+
 // test function toStrung()
 // ( fish ) -> ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
 console.log(list.toString());
+
+// test funciotn size()
+// Total number of nodes: 7
+console.log(`Total number of nodes: ${list.size()}`);
+
+// test function head()
+// The first node: ( fish )
+console.log(`The first node: ${list.head()}`);
+
+// test function tail()
+// The first node: ( turtle )
+console.log(`The last node: ${list.tail()}`);
+
+// test function at()
+//The node at index 5: ( hamster )
+console.log(list.at("javascript"));
+console.log(list.at(0));
+console.log(list.at(8));
+console.log(`The node at index 5: ${list.at(5)}`);
